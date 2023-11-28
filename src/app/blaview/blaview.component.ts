449ac441fb8687df58b30b7632fab75e723f586d
Augustin Preda <thegusty999@gmail.com>
2023-11-28 22:27:27 +0200

    make sortable columns

--- src/app/blaview/blaview.component.ts
+++ src/app/blaview/blaview.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BlaService, Player } from './bla.service';
-import { BehaviorSubject, combineLatest, map, shareReplay, tap } from 'rxjs';
+import { BehaviorSubject, combineLatest, map, scan, shareReplay, tap } from 'rxjs';
 
 export enum SortDirection {
   Ascending = 'asc',
   Descending = 'desc',
   None = ''
+}
+
+export interface SortColumnConfig {
+  name: string;
+  sortable: boolean;
 }
 
 export interface SortCriteria {
@@ -22,6 +27,17 @@ export interface SortCriteria {
   styleUrl: './blaview.component.css'
 })
 export class BlaviewComponent {
+
+  private columnDefs = [
+    { name: "id", sortable: true },
+    { name: "name", sortable: true },
+    { name: "extraSearchFields", sortable: true },
+    { name: "rating", sortable: true },
+    { name: "comments", sortable: true },
+  ] as SortColumnConfig[];
+
+  private cachedSortCriteria: SortCriteria = { sortField: '', sortDirection: SortDirection.None };
+
   sortPlayers(players: Player[], sortCriteria: SortCriteria): any {
     if (sortCriteria.sortDirection === SortDirection.None) {
       return players;
@@ -40,20 +56,90 @@ export class BlaviewComponent {
   sortCriteriaSubject$ = new BehaviorSubject<SortCriteria>({ sortField: '', sortDirection: SortDirection.None });
   sortCriteria$ = this.sortCriteriaSubject$.asObservable();
 
+  changeSortCriteriaSubject$ = new BehaviorSubject<SortCriteria>({ sortField: '', sortDirection: SortDirection.None });
+  changeSortCriteria$ = this.changeSortCriteriaSubject$.asObservable().pipe(
+    tap(item => console.log('*** changeSortCriteria: ', item)),
+    // adjust from the previous value (toggle it).
+    scan((previous, current) => {
+      if (previous.sortField === current.sortField) {
+        if (previous.sortDirection === SortDirection.Ascending) {
+          return { sortField: current.sortField, sortDirection: SortDirection.Descending };
+        } else if (previous.sortDirection === SortDirection.Descending) {
+          return { sortField: current.sortField, sortDirection: SortDirection.None };
+        } else {
+          return { sortField: current.sortField, sortDirection: SortDirection.Ascending };
+        }
+      } else {
+        return { sortField: current.sortField, sortDirection: SortDirection.Ascending };
+      }
+    }),
+    tap(item => this.cachedSortCriteria = item),
+    shareReplay(1),
+
+  );
+
   players$ = combineLatest([
     this.dataService.players$,
-    this.sortCriteria$
+    this.changeSortCriteria$
   ]).pipe(
-    tap(([players, sortCriteria]) => { console.log('*** players: ', players, 'sortCriteria: ', sortCriteria);}),
+    tap(([players, sortCriteria]) => { console.log('*** players: ', players, 'sortCriteria: ', sortCriteria); }),
     map(([players, sortCriteria]) => {
       return this.sortPlayers(players, sortCriteria);
     }),
     shareReplay(1),
   );
 
-  onHeaderClicked(field: string) {
-    console.log('*** onHeaderClicked: ', field);
+  onHeaderClicked(field: string, event$: any) {
+    console.log('*** onHeaderClicked: ', field, event$);
+    this.changeSortCriteriaSubject$.next({ sortField: field, sortDirection: SortDirection.Ascending });
+
+    var target = event$.target || event$.srcElement || event$.currentTarget;
+    console.log(" target: ", target);
+    var attr = target.attributes.getNamedItem("mapfield");
+    console.log(" attr: ", attr);
+    // var value = idAttr.nodeValue;
+    // console.log(" target: ", target, " idAttr: ", idAttr, " value: ", value);
     
-    this.sortCriteriaSubject$.next({ sortField: field, sortDirection: SortDirection.Ascending });
+  }
+
+  getCSSClassForTableHeader(field: string): string {
+    // If not sortable, return empty string
+    if (this.columnDefs.find(col => col.name === field)?.sortable === false) {
+      return "";
+    }
+
+    if (this.cachedSortCriteria.sortField != field) {
+      return "sortable both";
+    }
+
+    if (this.cachedSortCriteria.sortDirection == SortDirection.Ascending) {
+      return "sortable asc";
+    }
+    if (this.cachedSortCriteria.sortDirection == SortDirection.Descending) {
+      return "sortable desc";
+    }
+
+    return "sortable both";
+  }
+
+  getCSSForTHAsync(field: string, crit: SortCriteria | null): string {
+    console.log('*** getCSSClassForTableHeader2: ', field, crit);
+    // If not sortable, return empty string
+    if (this.columnDefs.find(col => col.name === field)?.sortable === false) {
+      return "";
+    }
+
+    if (crit && crit.sortField != field) {
+      return "sortable both";
+    }
+
+    if (crit && crit.sortDirection === SortDirection.Ascending) {
+      return "sortable asc";
+    }
+    if (crit && crit.sortDirection === SortDirection.Descending) {
+      return "sortable desc";
+    }
+
+    return "sortable both";
   }
 }
